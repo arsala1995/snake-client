1a2946e1cf11e54229160c670ff7f13f5ba00f61
Sher Shah Arsalaie <shershah.arsalaie@hotmail.com>
2020-09-23 14:31:39 -0700

    after editing play and client

--- client.js
+++ client.js
@@ -10,11 +10,20 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
   
+  conn.on('connect', () => {
+    // when a connection is established
+    console.log("successfully connected to server");
+    conn.write("Name: Ars");
+    });
+
   conn.on('data', (data) => {
       console.log('Server says: ', data);
       console.log("It's got a sharp tongue, this server!");
+     
     });
    
     
   return conn;
 }
+
+module.exports = { connect };
