a7a8794fc8795c5672982510706cf0d9acb73f54
Sher Shah Arsalaie <shershah.arsalaie@hotmail.com>
2020-09-23 16:45:47 -0700

    adding values

--- client.js
+++ client.js
@@ -1,46 +1,86 @@
-const net = require('net');
 
-const connect = function() {
-  const conn = net.createConnection({ 
+// const  setupInput  = require('./play.js').setupInput;
+// console.log(setupInput);
+
+// const setupInput = function () {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding('utf8');
+//   stdin.resume();
+//   return stdin;
+// }
+
+// console.log("setup:" + setupInput);
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput();
+  return stdin;
+}
+
+const handleUserInput = function() {
+  stdin.on('data', (data) => {
+    if (data == '\u0003') {
+    process.exit();
+    }
+    
+    });
+}
+
+const { Console } = require('console');
+const net = require('net');
+const stdin = process.stdin;
+
+const connect = function () {
+  const conn = net.createConnection({
     host: 'localhost',
     port: 50541
 
-   
+
   });
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  
+  conn.setEncoding('utf8');
+
   conn.on('connect', () => {
     // when a connection is established
     console.log("successfully connected to server");
     conn.write("Name: Ars");
 
-    setTimeout(() => {
-      conn.write("Move: up");
-    }, 50);
-    setTimeout(() => {
-      conn.write("Move: left");
-    }, 100);
-    setTimeout(() => {
-      conn.write("Move: down");
-    }, 150);
-    setTimeout(() => {
-      conn.write("Move: right");
-    }, 200);
-    
-    setInterval(() => {
-      conn.write("Move: up");
-    }, 50);
-   
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 50);
+    // setTimeout(() => {
+    //   conn.write("Move: left");
+    // }, 100);
+    // setTimeout(() => {
+    //   conn.write("Move: down");
+    // }, 150);
+    // setTimeout(() => {
+    //   conn.write("Move: right");
+    // }, 200);
+
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 50);
+    const setupInput1 = setupInput();
+    setupInput1.on('data', (data) => {
+      
+
+       console.log("the new data: " + data);
+       conn.write(data);
     });
+ 
+  });
 
   conn.on('data', (data) => {
-      console.log('Server says: ', data);
-      console.log("It's got a sharp tongue, this server!");
-     
-    });
-   
-    
+    console.log('Server says: ', data);
+    console.log("It's got a sharp tongue, this server!");
+
+  });
+
+
   return conn;
 }
 
--- play.js
+++ play.js
@@ -1,5 +1,40 @@
 const { connect } = require('./client');
 
-console.log('Connecting ...');
+// const setupInput = function() {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding('utf8');
+//   stdin.resume();
+//   handleUserInput();
+//   return stdin;
+// }
+
+// const handleUserInput = function() {
+//   stdin.on('data', (data) => {
+//     if (data == '\u0003') {
+//     process.exit();
+//     }
+    
+//     });
+// }
+ console.log('Connecting ...');
 
 connect();
+// console.log(setupInput);
+// module.exports = { setupInput };
+
+
+// module.exports = setupInput;
+
+
+// setupInput1.on('data', (data) => {
+//   if (data == "\u0003") {
+//         process.exit();
+//         }
+
+//         else if{
+
+//         }
+//    console.log("the new data: " + data);
+//    conn.write(data);
+// });
