ca4d8df788c38c042f1a0fd9cd98fc0d00d0b299
Sher Shah Arsalaie <shershah.arsalaie@hotmail.com>
2020-09-23 19:48:28 -0700

    creating new constants file

--- client.js
+++ client.js
@@ -1,14 +1,12 @@
+const { IP, PORT } = require('./constants');
 const { setupInput } = require('./input');
-
-const { Console } = require('console');
 const net = require('net');
-const stdin = process.stdin;
+
 
 const connect = function () {
   const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-
+    host: IP,
+    port: PORT
 
   });
   // interpret incoming data as text
@@ -18,47 +16,30 @@ const connect = function () {
     // when a connection is established
     console.log("successfully connected to server");
     conn.write("Name: Ars");
+    conn.write("Say: HI")
 
-    // setTimeout(() => {
-    //   conn.write("Move: up");
-    // }, 50);
-    // setTimeout(() => {
-    //   conn.write("Move: left");
-    // }, 100);
-    // setTimeout(() => {
-    //   conn.write("Move: down");
-    // }, 150);
-    // setTimeout(() => {
-    //   conn.write("Move: right");
-    // }, 200);
-
-    // setInterval(() => {
-    //   conn.write("Move: up");
-    // }, 50);
     const setupInput1 = setupInput();
 
     setupInput1.on('data', (data) => {
-      if (data == 'w'){
+      if (data == 'w') {
         conn.write("Move: up");
         console.log("up");
       }
-      else if (data == 's'){
+      else if (data == 's') {
         conn.write("Move: down");
         console.log("down");
       }
-     else if (data == 'a'){
+      else if (data == 'a') {
         conn.write("Move: left");
         console.log("left");
       }
-      else if (data == 'd'){
+      else if (data == 'd') {
         conn.write("Move: right");
         console.log("right");
       }
 
-      //  console.log("the new data: " + data);
-      //  conn.write(data);
     });
- 
+
   });
 
   conn.on('data', (data) => {
